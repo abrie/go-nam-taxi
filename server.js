f18bd34eeae221754c518ec431afa650e0e0e5d2
abrie <abrhie@gmail.com>
2015-09-30 08:31:11 -0400

    fixed bad merge.

--- server.js
+++ server.js
@@ -1,29 +1,3 @@
-<<<<<<< HEAD
-var secrets = tryRequire('./private/api_keys');
-var shortid = require('shortid');
-var fs = require('fs');
-var http = require('http');
-var dispatch = require('dispatch');
-var WebSocketServer = require('ws').Server;
-var Plates = require('plates');
-var wss = new WebSocketServer({ port:8080 });
-var clients = new Clients();
-
-wss.on('connection', function (socket) {
-    var client = new Client(socket);
-    clients.add(client); 
-    socket.on('close', function close() { 
-        clients.remove(client); 
-        console.log("bye client: %s", client.id);
-    });
-
-    client.sendHello();
-    console.log("new client: %s", client.id);
-});
-
-var server = http.createServer(
-    dispatch({
-=======
 #!/usr/bin/env node
 
 var Dispatch = require('dispatch');
@@ -48,31 +22,20 @@ socketServer.on('connection', function(socket) {
 
 var server = Http.createServer(
     Dispatch({
->>>>>>> dev
         '/client': serveClient,
         '.*': serve404
     })
 );
 
 function serveClient(req, res) {
-<<<<<<< HEAD
-    fs.readFile('client.html', 'utf8', function (err, html) {
-=======
     Fs.readFile('client.html', 'utf8', function(err, html) {
->>>>>>> dev
         if (err) {
             serve500(req, res);
             console.log("%s", err);
         }
         else {
-<<<<<<< HEAD
-            var output = Plates.bind(html, secrets);
-            res.writeHead(200, {'Content-Type': 'text/html'});
-            res.write(output);
-=======
             res.writeHead(200, {'Content-Type': 'text/html'});
             res.write(html);
->>>>>>> dev
             res.end();
         }
     });
@@ -103,11 +66,7 @@ function tryRequire(path) {
 function Client(socket) {
     var fields = {
         socket: socket,
-<<<<<<< HEAD
-        id: shortid.generate(),
-=======
         clientId: ShortId.generate(),
->>>>>>> dev
         sendHello: sendHello,
         sendUpdate: sendUpdate,
     }
@@ -119,52 +78,32 @@ function Client(socket) {
     }
 
     function sendHello() {
-<<<<<<< HEAD
-        var raw = JSON.stringify({id:fields.id, type:0});
-=======
         var raw = JSON.stringify({
             type: 0,
             clientId: fields.clientId, 
             google_maps_browser_key: Secrets.google_maps_browser_key,
         });
 
->>>>>>> dev
         socket.send(raw, ackHandler);
     }
 
     function sendUpdate(content) {
-<<<<<<< HEAD
-        var raw = JSON.stringify({id:fields.id, type:1, content:content});
-=======
         var raw = JSON.stringify({
             type: 1, 
             clientId: fields.clientId, 
             content: content
         });
 
->>>>>>> dev
         socket.send(raw, ackHandler);
     }
 
     socket.on('message', function incoming(message) {
-<<<<<<< HEAD
-        console.log("client say: %s : %s", fields.id, message);
-=======
         console.log("client say: %s : %s", fields.clientId, message);
->>>>>>> dev
     });
 
     return fields;
 }
 
-<<<<<<< HEAD
-function Clients() {
-    var clients = [];
-    var updateCounter = 0;
-
-    function add(client) {
-        clients.push(client);
-=======
 function ClientManager() {
     var clients = [];
     var updateCounter = 0;
@@ -174,16 +113,12 @@ function ClientManager() {
         clients.push(newClient);
         console.log("new client: %s", newClient.clientId);
         return newClient;
->>>>>>> dev
     }
 
     function remove(client) {
         var index = clients.indexOf(client);
         var removed_client = clients.splice(index, 1);
-<<<<<<< HEAD
-=======
         console.log("bye client: %s", client.clientId);
->>>>>>> dev
     }
 
     function tick() {
@@ -193,26 +128,12 @@ function ClientManager() {
     }
 
     return {
-<<<<<<< HEAD
-        add:add,
-=======
         createNewClient:createNewClient,
->>>>>>> dev
         remove:remove,
         tick: tick
     }
 }
 
-<<<<<<< HEAD
-server.listen(8081);
-console.log("#GoNamTaxi2015 Prototype Server");
-
-function broadcast() {
-    clients.tick();
-}
-
-broadcastTimer = setInterval(broadcast, 500);
-=======
 function buildRequest(baseUrl, params) {
     var result = undefined;
 
@@ -254,4 +175,3 @@ function snapToRoads(path) {
 
 console.log("#GoNamTaxi2015 Prototype Server");
 server.listen(8081);
->>>>>>> dev
