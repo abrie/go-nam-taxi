9d8b31fa63ad482e1d1d5f8944491227d82ba130
abrie <abrhie@gmail.com>
2015-10-02 17:08:12 -0400

    refactor: remove imageloader because unnecessary

--- android/TillApp/app/src/main/java/na/nbii/tillapp/NetComm.java
+++ android/TillApp/app/src/main/java/na/nbii/tillapp/NetComm.java
@@ -3,39 +3,19 @@
 package na.nbii.tillapp;
 
 import android.content.Context;
-import android.graphics.Bitmap;
-import android.support.v4.util.LruCache;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
-import com.android.volley.toolbox.ImageLoader;
 import com.android.volley.toolbox.Volley;
 
 public class NetComm {
     private static NetComm mInstance;
     private RequestQueue mRequestQueue;
-    private ImageLoader mImageLoader;
     private static Context mCtx;
 
     private NetComm(Context context) {
         mCtx = context;
         mRequestQueue = getRequestQueue();
-
-        mImageLoader = new ImageLoader(mRequestQueue,
-                new ImageLoader.ImageCache() {
-                    private final LruCache<String, Bitmap>
-                            cache = new LruCache<String, Bitmap>(20);
-
-                    @Override
-                    public Bitmap getBitmap(String url) {
-                        return cache.get(url);
-                    }
-
-                    @Override
-                    public void putBitmap(String url, Bitmap bitmap) {
-                        cache.put(url, bitmap);
-                    }
-                });
     }
 
     public static synchronized NetComm getInstance(Context context) {
@@ -56,9 +36,5 @@ public class NetComm {
 
     public <T> void addToRequestQueue(Request<T> req) {
         getRequestQueue().add(req);
-    }
-
-    public ImageLoader getImageLoader() {
-        return mImageLoader;
     }
 }
