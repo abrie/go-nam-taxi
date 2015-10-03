ef91e489b2bb4b897ec4d043bf6b8500df1cf0e5
abrie <abrhie@gmail.com>
2015-10-02 21:16:42 -0400

    rename cash payment button 'fab'->'cash_button'

--- android/TillApp/app/src/main/java/na/nbii/tillapp/MainActivity.java
+++ android/TillApp/app/src/main/java/na/nbii/tillapp/MainActivity.java
@@ -18,7 +18,7 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.cash_button);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(final View view) {
