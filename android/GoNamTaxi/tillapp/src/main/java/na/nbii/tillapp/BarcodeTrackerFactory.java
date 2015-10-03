5e87dd3d9283a9a835accdb8611a8ebfbf5eb812
abrie <abrhie@gmail.com>
2015-10-03 09:20:28 -0400

    incremental: reference commit for barcode scanning

---
+++ android/GoNamTaxi/tillapp/src/main/java/na/nbii/tillapp/BarcodeTrackerFactory.java
@@ -0,0 +1,28 @@
+package na.nbii.tillapp;
+
+/**
+ * Created by abrie on 15-10-03.
+ */
+
+import com.google.android.gms.vision.MultiProcessor;
+import com.google.android.gms.vision.Tracker;
+import com.google.android.gms.vision.barcode.Barcode;
+
+/**
+ * Factory for creating a tracker and associated graphic to be associated with a new barcode.  The
+ * multi-processor uses this factory to create barcode trackers as needed -- one for each barcode.
+ */
+class BarcodeTrackerFactory implements MultiProcessor.Factory<Barcode> {
+    private GraphicOverlay<BarcodeGraphic> mGraphicOverlay;
+
+    BarcodeTrackerFactory(GraphicOverlay<BarcodeGraphic> barcodeGraphicOverlay) {
+        mGraphicOverlay = barcodeGraphicOverlay;
+    }
+
+    @Override
+    public Tracker<Barcode> create(Barcode barcode) {
+        BarcodeGraphic graphic = new BarcodeGraphic(mGraphicOverlay);
+        return new BarcodeGraphicTracker(mGraphicOverlay, graphic);
+    }
+
+}
