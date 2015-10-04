2695510e8602a203c8c6ea3bf2b8b045185f2d73
abrie <abrhie@gmail.com>
2015-10-04 08:20:49 -0400

    replace floating buttons with regular buttons

--- android/GoNamTaxi/tillapp/src/main/java/na/nbii/tillapp/MainActivity.java
+++ android/GoNamTaxi/tillapp/src/main/java/na/nbii/tillapp/MainActivity.java
@@ -5,7 +5,6 @@ import android.content.SharedPreferences;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
-import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -14,6 +13,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 
 import com.google.android.gms.vision.barcode.Barcode;
@@ -91,8 +91,8 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.cash_button);
-        fab.setOnClickListener(new View.OnClickListener() {
+        Button cashButton = (Button)findViewById(R.id.pay_cash);
+        cashButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(final View view) {
                 NetRequestQueue.getInstance(getApplicationContext())
@@ -113,8 +113,8 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        FloatingActionButton coupon = (FloatingActionButton) findViewById(R.id.coupon_button);
-        coupon.setOnClickListener(new View.OnClickListener() {
+        Button ticketButton = (Button)findViewById(R.id.scan_ticket);
+        ticketButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(final View view) {
                 // launch barcode activity.
